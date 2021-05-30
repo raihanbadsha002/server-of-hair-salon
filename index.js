66a805a23e596efd2a63edc81d87002c284c8502
RAIHAN <raihanbadsha363@gmail.com>
2021-05-30 23:51:36 +0600

    server-code

--- index.js
+++ index.js
@@ -109,5 +109,5 @@ app.get('/orderList', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`http://localhost:${port}`)
   })
